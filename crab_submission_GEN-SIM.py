4d2c2d5f1e03845d381e702170772d2e5d04ba66
Olmo Cerri <olmo.cerri@gmail.com>
2018-10-08 18:19:35 -0700

    v0 working

--- crab_submission_GEN-SIM.py
+++ crab_submission_GEN-SIM.py
@@ -1,23 +1,22 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName     = 'BToKmm_GEN-SIM_18_03_20'
-config.General.workArea        = 'BToKmm'
+config.General.requestName     = 'HardQCD_bbar_Bu_D0munu_KPimunu_GEN-SIM_18_10_08'
+config.General.workArea        = 'HardQCD_bbar_Bu_D0munu_KPimunu'
 config.General.transferOutputs = True
 config.General.transferLogs    = True
 
 config.JobType.pluginName = 'PrivateMC'
-config.JobType.psetName   = 'step1_BToKmm_GEN-SIM_cfg.py'
-config.JobType.eventsPerLumi = 5000
+config.JobType.psetName   = 'step1_HardQCD_bbar_Bu_D0munu_KPimunu_GEN-SIM_cfg.py'
 config.JobType.numCores = 1
 
-config.Data.outputPrimaryDataset = 'BToKmm_Pythia'
+config.Data.outputPrimaryDataset = 'HardQCD_bbar_Bu_D0munu_KPimunu_Pythia'
 config.Data.inputDBS             = 'global'
 config.Data.splitting            = 'EventBased'
-config.Data.unitsPerJob          = 500000
-config.Data.totalUnits           = 5000000000
+config.Data.unitsPerJob          = 10000
+config.Data.totalUnits           = 1500000
 config.Data.outLFNDirBase        = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication          = True
-config.Data.outputDatasetTag     = 'BToKmm_Pythia_GEN-SIM_18_03_20'
+config.Data.outputDatasetTag     = 'HardQCD_bbar_Bu_D0munu_KPimunu_GEN-SIM_18_10_08'
 
-config.Site.storageSite = 'T2_UK_London_IC'
+config.Site.storageSite = 'T2_US_Caltech'
--- run1023_gen_NoPU_v0.sh
+++ run1023_gen_NoPU_v0.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-process_name=Bu_D0munu_KPimunu
+process_name=HardQCD_bbar_Bu_D0munu_KPimunu
 version=NoPU_10-2-3_v0
 out_loc=/afs/cern.ch/user/o/ocerri/cernbox/BPhysics/data/cmsMC_private
-N_evts=1000
+N_evts=$1
+# N_evts=100000
 
 out_dir=$out_loc/${process_name}_$version
 MC_frag_file=/afs/cern.ch/user/o/ocerri/cernbox/BPhysics/MCGeneration/BPH_CMSMCGen/Configuration/GenProduction/python/${process_name}_13TeV-pythia8-evtgen_cfi.py
@@ -28,7 +29,7 @@ fi
 
 echo "Step 1: GEN-SIM"
 cd /afs/cern.ch/user/o/ocerri/cernbox/BPhysics/CMSSW_10_2_3/src
-# cmsenv
+
 eval `scramv1 runtime -sh`
 
 mkdir -p Configuration/GenProduction/python
@@ -37,11 +38,13 @@ cp $MC_frag_file Configuration/GenProduction/python/${process_name}_13TeV-pythia
 
 scram b -j9
 
+echo "cmsDriver.py Configuration/GenProduction/python/${process_name}_13TeV-pythia8-evtgen_cfi.py --fileout file:${process_name}_GEN-SIM.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 102X_upgrade2018_realistic_v12 --beamspot Realistic25ns13TeVEarly2018Collision --step GEN,SIM --nThreads 8 --geometry DB:Extended --era Run2_2018 --python_filename step1_${process_name}_GEN-SIM_cfg.py --no_exec -n $N_evts"
+
 cmsDriver.py Configuration/GenProduction/python/${process_name}_13TeV-pythia8-evtgen_cfi.py --fileout file:${process_name}_GEN-SIM.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 102X_upgrade2018_realistic_v12 --beamspot Realistic25ns13TeVEarly2018Collision --step GEN,SIM --nThreads 8 --geometry DB:Extended --era Run2_2018 --python_filename step1_${process_name}_GEN-SIM_cfg.py --no_exec -n $N_evts
 # --customise Configuration/DataProcessing/Utils.addMonitoring
 
 mv ./step1_${process_name}_GEN-SIM_cfg.py $out_dir/
-mv -r Configuration $out_dir/
+mv Configuration $out_dir/
 cd $out_dir
 
 cmsRun step1_${process_name}_GEN-SIM_cfg.py &> step1.log
@@ -53,13 +56,18 @@ echo "Step 2: RAW -> MINIAOD"
 
 cmsDriver.py --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 102X_upgrade2018_realistic_v12 --step DIGI,L1,DIGI2RAW,HLT:@relval2018 --nThreads 8 --era Run2_2018 --filein file:${process_name}_GEN-SIM.root --fileout file:${process_name}_RAW.root --python_filename step2_${process_name}_RAW_cfg.py --no_exec -n -1
 
-cmsRun step2_${process_name}_RAW_cfg.py
+cmsRun step2_${process_name}_RAW_cfg.py &> step2.log
+
+
 
 cmsDriver.py --filein file:${process_name}_RAW.root --fileout file:${process_name}_AODSIM.root --mc --eventcontent AODSIM runUnscheduled --datatier AODSIM --conditions 102X_upgrade2018_realistic_v12 --step RAW2DIGI,RECO,RECOSIM,EI --nThreads 8 --era Run2_2018 --python_filename step3_${process_name}_AODSIM_cfg.py --no_exec -n -1
-# --customise Configuration/DataProcessing/Utils.addMonitoring
 
-cmsRun step3_${process_name}_AODSIM_cfg.py
+cmsRun step3_${process_name}_AODSIM_cfg.py &> step3.log
+rm ${process_name}_RAW.root
+
+
 
 cmsDriver.py --filein file:${process_name}_AODSIM.root --fileout file:${process_name}_MINIAODSIM.root --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions 102X_upgrade2018_realistic_v12 --step PAT --era Run2_2018 --nThreads 8 --python_filename step4_${process_name}_MINIAODSIM_cfg.py --no_exec -n -1
 
-cmsRun step4_${process_name}_MINIAODSIM_cfg.py
+cmsRun step4_${process_name}_MINIAODSIM_cfg.py &> step4.log
+rm ${process_name}_AODSIM.root
